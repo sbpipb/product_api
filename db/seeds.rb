cbaecffd6c778d61892634f07b555015b190c7fa
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-05-31 22:55:26 +0800

    initial rails api 4.2.6 --api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+products = Product.create([{ name: 'Plate', price: 100.0, description: 'white plate, made in china' }, { name: 'Black Plate', price: 22.0, description: 'black plate, made in china' }])
