5a389bd1781a1ab680c25c9e7d9179823b5ded4f
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-01 11:53:01 +0800

    webpack integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
 
+var srcPath = path.join(__dirname, 'src');
 var distPath = path.join(__dirname, 'public');
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
+
+  // context: path.resolve(__dirname, './src'),
+
+
   entry: {
-    app: './js/app.js',
+    app: path.join(srcPath, 'js/app.js'),
+    // app: ['./home.js', './events.js', './vendor.js'],
+    vendor: ['react', 'react-dom'],
   },
   output: {
-    // path: path.resolve(__dirname, './dist'),
     path: distPath,
-    filename: 'js/[name]-[hash].js',
+    // path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name].js',
+    // filename: 'js/[name]-[hash].js',
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+     name: 'julian',
+     filename: 'js/[name]-vendor-[hash].js',
+     minChunks: 2,
+   }),
+  ],
 };
