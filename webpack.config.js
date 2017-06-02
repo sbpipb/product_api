e31f9f5f8e267baf2b73fc65c88ba4a936b04ff4
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-02 11:22:38 +0800

    added webpack plugs (html, common chunks)
    update on package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,78 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
 const path = require('path');
-const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var srcPath = path.join(__dirname, 'src');
 var distPath = path.join(__dirname, 'public');
 
+var vendorJS = [
+  'react',
+  'react-dom'
+  // path.join(srcPath, 'js/foundation.js'),
+];
+
 module.exports = {
-
-  // context: path.resolve(__dirname, './src'),
-
+  devtool: debug ? "inline-sourcemap" : false,
 
   entry: {
     app: path.join(srcPath, 'js/app.js'),
-    // app: ['./home.js', './events.js', './vendor.js'],
-    vendor: ['react', 'react-dom'],
+    vendor: vendorJS
   },
+
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015', 'stage-1', 'stage-2']
+            }
+          }
+        },
+        {
+          // Loaders are processed in reverse array order. That means css-loader will run before style-loader.
+          test: /\.css$/,
+          use: ['style-loader', {
+            loader: 'css-loader',
+            options: { modules: false },
+            }
+          ],
+        },
+      ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, './src'), 'node_modules']
+  },
+
   output: {
+    // library: 'myClassName',
     path: distPath,
-    // path: path.resolve(__dirname, './dist'),
-    filename: 'js/[name].js',
-    // filename: 'js/[name]-[hash].js',
+    filename: 'js/[name]-[hash].js',
+    chunkFilename: 'js/[id]-[hash].js',
+    publicPath: '/public/',
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+  },
+  // plugins: debug ? [] : [
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-     name: 'julian',
-     filename: 'js/[name]-vendor-[hash].js',
-     minChunks: 2,
-   }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+          name: 'vendor',
+          filename: 'js/vendor-[hash].js'
+        }),
+        // new webpack.optimize.OccurrenceOrderPlugin(),
+        //
+        // new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+
+        new HtmlWebpackPlugin({
+          inject: true,
+          template: 'src/index.html',
+          filename: 'index.html'
+        }),
   ],
+
 };
