805c83423baca69d9687f2da48d06feb46bcb804
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-03 14:03:36 +0800

    working index and app js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
 import moment from 'moment';
 var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(rightNow);
-console.log('app js');
+import Formsy from 'formsy-react';
+
+class Layout extends React.Component {
+  productListing = (props) => {
+    let elements = [1, 2, 3, 4].map( function(item) {
+       return <li key={item}>{item} </li>
+     })
+    return (
+      <ul>{elements}</ul>
+    )
+  }
+
+  defaultTemplate = (props) => {
+    let list = this.productListing()
+    return (
+      <div>
+        {list}
+      </div>
+
+    )
+  }
+
+  render() {
+    return (this.defaultTemplate())
+  }
+
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout />, app)
