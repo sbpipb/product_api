51732e42dfb21ea40cc2583a71e2a6396b185b6a
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-04 20:48:46 +0800

    product form

---
+++ src/js/components/productForm/Form.react.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ProductStore from "../../stores/productStore";
+import * as ProductActions from "../../actions/ProductActions";
+
+class ProductForm extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      price: '',
+      description: ''
+    }
+  }
+
+  submit() {
+
+  }
+
+  enableButton = () => {
+
+  }
+
+  disableButton = () => {
+
+  }
+
+  submitAction = (event) => {
+    let product = {
+      name: this.state.name,
+      price: this.state.price,
+      description: this.state.description,
+    }
+    ProductActions.createTodo(product);
+  }
+
+  changeName = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  changeDescription = (event) => {
+    this.setState({description: event.target.value});
+  }
+
+  changePrice = (event) => {
+    this.setState({price: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className='col-md-5'>
+        <h1>Create Product</h1>
+        <div>
+          <div className='row'>
+            <div className="form-group col-md-6">
+              <label canTor="exampleInputEmail1">Name</label>
+              <input onBlur={this.changeName.bind(this)}  type="name" className="form-control" id="exampleInputEmail1" placeholder="name" />
+            </div>
+
+            <div className="form-group col-md-6">
+              <label canTor="exampleInputPassword1">Price</label>
+              <input onBlur={this.changePrice.bind(this)}  type="name" className="form-control" id="exampleInputPrice1" placeholder="40.00" />
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='form-group col-md-6'>
+              <button className='btn' onClick={this.submitAction.bind(this)}  className="btn btn-default">Submit</button>
+            </div>
+            <div className="form-group col-md-6">
+              <label canTor="exDescription">Description</label>
+              <input onBlur={this.changeDescription.bind(this)}  type="description" className="form-control" id="exDescription" placeholder="item is blah blaa lbah" />
+            </div>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+export default ProductForm
