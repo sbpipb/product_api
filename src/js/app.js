62adcd5b80835d86776ef18143532473ecd0bdaa
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-04 19:54:20 +0800

    working listing via api

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import moment from 'moment';
-var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-import Formsy from 'formsy-react';
+import * as ProductActions from "./actions/ProductActions";
+import ProductStore from "./stores/productStore";
+import ProductForm from "./components/productForm";
 
 class Layout extends React.Component {
-  productListing = (props) => {
-    let elements = [1, 2, 3, 4].map( function(item) {
-       return <li key={item}>{item} </li>
+
+  constructor(){
+    super()
+    this.getProducts = this.getProducts.bind(this);
+    this.state = {
+      products: [],
+    };
+  }
+
+  getProducts() {
+    let products = ProductStore.getAll()
+    this.setState({
+      products: products
+    })
+  }
+
+  componentWillMount() {
+    ProductStore.on('change', this.getProducts);
+    ProductActions.getProducts();
+  }
+
+  componentWillUnmount() {
+    ProductStore.removeListener("change", this.getProducts);
+  }
+
+  productListing = (products) => {
+    let elements = products.map((product) => {
+      let { name, id, price, description} = product
+       return ( <tr key={id}><td>{name}</td>
+                             <td>${price}</td>
+                             <td>{description}</td>
+                </tr>
+              )
      })
-    return (
-      <ul>{elements}</ul>
-    )
+    return (elements)
   }
 
   defaultTemplate = (props) => {
-    let list = this.productListing()
+    const productListing = this.productListing(this.state.products)
     return (
       <div>
-        {list}
+
+      <div>
+       <Form state={this.state} onChange={changes => this.setState(changes)}>
+         <Field fieldName='name' label='Name' type={Text}/>
+         <Field fieldName='description' label='description' type={Text}/>
+         <Field fieldName='cost' label='cost' type={Text}/>
+       </Form>
       </div>
 
+
+
+        <div className="panel panel-default">
+            <table className="table table-hover ">
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Price</td>
+                <td>Description</td>
+              </tr>
+            </thead>
+            <tbody>
+              {productListing}
+            </tbody>
+            </table>
+        </div>
+
+      </div>
     )
   }
 
