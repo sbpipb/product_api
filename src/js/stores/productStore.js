1c0c7cfeaab132b909e8cc9785e416a64bb6fa30
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-04 19:52:43 +0800

    working product store

---
+++ src/js/stores/productStore.js
@@ -0,0 +1,66 @@
+import { EventEmitter } from "events";
+
+import dispatcher from "../dispatcher";
+
+class ProductStore extends EventEmitter {
+  constructor() {
+    super()
+  }
+
+  // createProduct(product) {
+  //   const id = Date.now();
+  //   this.products.push({
+  //     id,
+  //     product,
+  //     cost,
+  //   });
+  //   this.emit("change");
+  // }
+
+  getAll() {
+    return this.products;
+  }
+
+  handleActions(action) {
+    switch(action.type) {
+      // case "CREATE_PRODUCT": {
+      //   this.createProduct(action.text);
+      //   break;
+      // }
+
+      case "RECEIVE_PRODUCTS": {
+        this.products = action.products['data'];
+        this.emit("change");
+        break;
+      }
+
+      case 'GET_PRODUCTS': {
+        // this.loading
+        // busTrips.isLoading = true
+        // searchTrip: function (params, callback) {
+        //   Request.get('/api/v2/travel/buses/trip/search', params, callback)
+        // },
+
+        // BusTrip.searchTrip(data, (error, response) => {
+        //   busTrips.isLoading = false
+        //   busTrips.hasError = !!error
+        //   busTrips._availableTrips = []
+        //
+        //   if(!busTrips.hasError && response.body.data !== null ) {
+        //     busTrips.availableTrips = response.body['data']
+        //     busTrips.hasError = !!!response.body['success']
+        //   }
+        //
+        //   this.emit('change');
+        // })
+
+
+      }
+    }
+  }
+
+}
+
+const productStore = new ProductStore;
+dispatcher.register(productStore.handleActions.bind(productStore));
+export default productStore;
