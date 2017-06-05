eeffef42be47a186bd424e7b944efa67e18667b6
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-05 16:45:23 +0800

    produc action cleanup

--- src/js/actions/ProductActions.js
+++ src/js/actions/ProductActions.js
@@ -2,19 +2,19 @@ import dispatcher from "../dispatcher";
 import axios from "axios";
 
 export function createProduct(product) {
-  dispatcher.dispatch({
-    type: "CREATING_PRODUCT"
-  });
+  // dispatcher.dispatch({
+  //   type: "CREATING_PRODUCT"
+  // });
 
   axios.post("http://127.0.0.1:5000/api/v1/products", product)
        .then((data) => {
-         dispatcher.dispatch({type: "CREATED_PRODUCT", product: data})
-        //  console.log(data)
+         dispatcher.dispatch({type: "CREATED_PRODUCT", product: data}, (){
+           
+         })
         })
       .catch((error) => {
         console.log(error)
       })
-
 }
 
 export function deleteProduct(id) {
@@ -25,7 +25,7 @@ export function deleteProduct(id) {
 }
 
 export function getProducts() {
-  dispatcher.dispatch({type: "FETCH_PRODUCTS"});
+  // dispatcher.dispatch({type: "FETCH_PRODUCTS"});
   axios("http://127.0.0.1:5000/api/v1/products").then((data) => {
     dispatcher.dispatch({type: "RECEIVE_PRODUCTS", products: data})
   })
