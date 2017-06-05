09633a09396bbd4f149dd804dda07cfeafd20f47
jvlivnmvcmvng <julianmacmang@gmail.com>
2017-06-05 17:56:55 +0800

    remove useless auth

--- app/controllers/base_api_controller.rb
+++ app/controllers/base_api_controller.rb
@@ -5,18 +5,18 @@ class BaseApiController < ApplicationController
 
   private
 
-  def authenticate_user_from_token!
-    if !@json['api_token']
-      render nothing: true, status: :unauthorized
-    else
-      @user = nil
-      User.find_each do |u|
-        if Devise.secure_compare(u.api_token, @json['api_token'])
-          @user = u
-        end
-      end
-    end
-  end
+  # def authenticate_user_from_token!
+  #   if !@json['api_token']
+  #     render nothing: true, status: :unauthorized
+  #   else
+  #     @user = nil
+  #     User.find_each do |u|
+  #       if Devise.secure_compare(u.api_token, @json['api_token'])
+  #         @user = u
+  #       end
+  #     end
+  #   end
+  # end
 
   def parse_request
     body = request.body.read
